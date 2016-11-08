5bb3c85d5898c03813efb43caa4c254d4d60c87c
csb <codysblanchard@gmail.com>
2016-11-08 09:41:22 -0800

    Db stuff

---
+++ sql.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.1.16-MariaDB : Database - emojiquest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`emojiquest` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `emojiquest`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(11) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(32) COLLATE utf8_bin DEFAULT NULL,
+  `x` tinyint(3) unsigned DEFAULT '0',
+  `y` tinyint(3) unsigned DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `users` */
+
+/*Table structure for table `zones` */
+
+DROP TABLE IF EXISTS `zones`;
+
+CREATE TABLE `zones` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `x` tinyint(3) unsigned DEFAULT NULL,
+  `y` tinyint(3) unsigned DEFAULT NULL,
+  `bg1` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `bg2` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `bg3` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `encounter` int(11) DEFAULT NULL,
+  `face` varchar(64) COLLATE utf8_bin DEFAULT NULL,
+  `fatigue` tinyint(4) DEFAULT NULL,
+  `minfatigue` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `zones` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
