0474cb5d3ae3ef61cd3f8dab4dce2130ce73cdc4
codysblanchard <codysblanchard@gmail.com>
2016-11-09 16:35:00 -0800

    nosie gen map

---
+++ mapgen.js
@@ -0,0 +1,34 @@
+var perlin = require('perlin-noise');
+const app = require('express')();
+const fs = require('fs');
+var _ = require('lodash');
+
+var xsize=99;
+var ysize=99;
+
+app.listen(3000,()=>{console.log('listening...')});
+app.get('/',(req,res)=>{
+    var map = perlin.generatePerlinNoise(xsize, ysize,{octaveCount:5,amplitude:.1,persistence:.5});
+    fs.writeFile("./map.js",map);
+    var y = 0;
+    res.send("<table cellpadding='0' cellspacing='0'>"+_.map(map,function(m,index){
+        var r='';
+        var x = index % 99;
+        if(index==0 || x == 0){
+            r+="</tr><tr>";
+            y++;
+        }
+
+        var color=Math.round((m*255)
+            -
+            ((Math.abs((xsize/2)-x)/xsize))*255
+            -
+            ((Math.abs((ysize/2)-y)/ysize))*255
+            )
+            ;
+        if(color<0)color=0;
+        var color = color.toString(16);
+        r+="<td width='20' bgcolor='"+color+""+color+""+color+"'>&nbsp;</td>";
+        return r;
+    }))
+});
